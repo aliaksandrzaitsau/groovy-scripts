0dd1d6350c5242d7931f7b5f633410974f400c06
aliaksandrzaitsau <35954552+aliaksandrzaitsau@users.noreply.github.com>
2018-03-27 14:48:31 +0300

    commited

---
+++ script_9.2.groovy
@@ -0,0 +1,29 @@
+import jenkins.model.Jenkins;
+import hudson.model.FreeStyleProject;
+import hudson.plugins.git.GitSCM;
+import hudson.tasks.Shell;
+import hudson.plugins.ws_cleanup.*
+
+//Get instance of Jenkins
+def parent = Jenkins.getInstance()
+
+//Define a job name
+//def jobName = "Job"
+
+//Instantiate a new project
+//def project = new FreeStyleProject(parent, jobName);
+
+//Define a branch name
+def branchName = "**/azaitsau"
+
+//Create a parameter for the project
+job = Jenkins.instance.createProject(FreeStyleProject, 'jobName')
+def url = "https://github.com/MNT-Lab/groovy-scripts.git"
+def gitScm = new GitSCM(url)
+gitScm.branches = [new hudson.plugins.git.BranchSpec(branchName)]
+job.scm = gitScm
+
+job.buildWrappersList.add(new PreBuildCleanup(null, true, "", ""))
+job.buildersList.add(new Shell('echo hello'))
+
+parent.reload()
