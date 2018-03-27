adba5d0e15156e584c0f1caebc661bed5f9db125
Alexey <shumilovy@mail.ru>
2018-03-27 15:09:53 +0300

    Update task_9.2.groovy

--- task_9.2.groovy
+++ task_9.2.groovy
@@ -2,8 +2,9 @@ import jenkins.model.Jenkins;
 import hudson.model.FreeStyleProject;
 import hudson.plugins.git.GitSCM;
 import hudson.tasks.Shell;
+import hudson.plugins.ws_cleanup.*
 
-job = Jenkins.instance.createProject(FreeStyleProject, 'JobProject_1')
+job = Jenkins.instance.createProject(FreeStyleProject, 'JobProject')
 
 def url = "https://github.com/MNT-Lab/build-principals.git/"
 def gitScm = new GitSCM(url)
@@ -12,3 +13,5 @@ job.scm = gitScm
 
 job.buildersList.add(new Shell('Hello world!'))
 job.save()
+
+job.buildWrappersList.add(new PreBuildCleanup(null, true, "", ""))
